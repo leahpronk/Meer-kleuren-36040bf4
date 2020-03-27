aebfaacdd33701be602747338f70eb46d279ee3b
Leah Pronk <2109208@talnet.nl git config --global user.name Leah>
2020-03-27 11:39:53 +0100

    Uitwerking van de exercise

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-function color() {
+function backKleur() {
     var element = document.getElementById("body");
-    element.style.backgroundColor = document.getElementById("color").value;
+    element.style.backgroundColor = document.getElementById("backKleur").value;
+}
+function tekstKleur() {
+  var texten = document.getElementById("hoofd");
+  texten.style.color = document.getElementById("tekstKleur").value;
 }
